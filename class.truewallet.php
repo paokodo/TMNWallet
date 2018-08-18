ddcf3806d2edf7a9eca07c884f8c67fc763a2a12
Maythiwat Chomchuen <maythiwat.chom@gmail.com>
2018-08-18 17:50:19 +0700

    Improvement

--- class.truewallet.php
+++ class.truewallet.php
@@ -9,7 +9,8 @@ class TrueWallet {
 	private $api_profile = "https://api-ewm.truemoney.com/api/v1/profile/";
 	private $api_topup = "https://api-ewm.truemoney.com/api/api/v1/topup/mobile/";
 	private $api_gettran = "https://api-ewm.truemoney.com/api/v1/profile/transactions/history/";
-	private $api_checktran = "https://api-ewm.truemoney.com/api/v1/profile/activities/";
+	private $api_getreport = "https://api-ewm.truemoney.com/api/v1/profile/activities/";
+
 	private $device_os = "android";
 	private $device_id = "d520d0d12d0d48cb89394905168c6ed5";
 	private $device_type = "CPH1611";
@@ -18,8 +19,8 @@ class TrueWallet {
 	private $app_version = "4.0.1";
 	private $deviceToken = "fUUbZJ9nwBk:APA91bHHgBBHhP9rqBEon_BtUNz3rLHQ-sYXnezA10PRSWQTwFpMvC9QiFzh-CqPsbWEd6x409ATC5RVsHAfk_-14cSqVdGzhn8iX2K_DiNHvpYfMMIzvFx_YWpYj5OaEzMyIPh3mgtx";
 	private $mobileTracking = "dJyFzn\/GIq7lrjv2RCsZbphpp0L\/W2+PsOTtOpg352mgWrt4XAEAAA==";
-    
-	public function __construct($user,$pass,$type) {
+
+	public function __construct($user, $pass, $type = 'email') {
 		$this->username = $user;
 		$this->password = $pass;
 		$this->login_type = $type;
@@ -40,34 +41,34 @@ class TrueWallet {
 			"mobileTracking"=>$this->mobileTracking,
 		);
 		$data_string = json_encode($postfield);
-		return $this->wallet_curl($url,$data_string,$header);
+		return $this->WalletCurl($url, $data_string, $header);
 	}
 
-	public function Profile($token) {
-		$url = $this->api_profile.$token.'?&device_os=android&device_id='.$this->device_id.'&device_type='.$this->device_type.'&device_version='.$this->device_version.'&app_name='.$this->app_name.'&app_version='.$this->app_version;
+	public function GetProfile($token) {
+		$url = $this->api_profile.$token.'?&device_os='.$this->device_os.'&device_id='.$this->device_id.'&device_type='.$this->device_type.'&device_version='.$this->device_version.'&app_name='.$this->app_name.'&app_version='.$this->app_version;
 		$header = array("Host: api-ewm.truemoney.com");
-		return $this->wallet_curl($url,false,$header);
+		return $this->WalletCurl($url, false, $header);
 	}
 
-	public function Topup($cashcard,$token) {
-		$url = $this->api_topup.time()."/".$token."/cashcard/".$cashcard;
-		$header = array("Host: api-ewm.truemoney.com");
-		return $this->wallet_curl($url,true,$header);
-	}
-
-	public function getTran($token,$start,$end) {
+	public function GetTransaction($token, $start, $end) {
 		$url = $this->api_gettran.$token.'/?startDate='.$start.'&endDate='.$end.'&limit=20&page=1&type=&action=';
 		$header = array("Host: api-ewm.truemoney.com");
-		return $this->wallet_curl($url,false,$header);
+		return $this->WalletCurl($url, false, $header);
 	}
 
-	public function CheckTran($token,$id) {
-		$url = $this->api_checktran.$id.'/detail/'.$token.'?&device_os=android&device_id='.$this->device_id.'&device_type='.$this->device_type.'&device_version='.$this->device_version.'&app_name='.$this->app_name.'&app_version='.$this->app_version;
+	public function GetReport($token, $id) {
+		$url = $this->api_getreport.$id.'/detail/'.$token.'?&device_os='.$this->device_os.'&device_id='.$this->device_id.'&device_type='.$this->device_type.'&device_version='.$this->device_version.'&app_name='.$this->app_name.'&app_version='.$this->app_version;
 		$header = array("Host: api-ewm.truemoney.com");
-		return $this->wallet_curl($url,false,$header);
+		return $this->WalletCurl($url, false, $header);
+	}
+    
+    public function Topup($cashcard,$token) {
+		$url = $this->api_topup.time()."/".$token."/cashcard/".$cashcard;
+		$header = array("Host: api-ewm.truemoney.com");
+		return $this->WalletCurl($url, true, $header);
 	}
 
-	private function wallet_curl($url,$data,$header) {
+	private function WalletCurl($url, $data, $header) {
 		$ch = curl_init();
 		curl_setopt($ch,CURLOPT_URL,$url);
 		curl_setopt($ch,CURLOPT_HTTPHEADER,$header);
