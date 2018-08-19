e32439d57b5b1de61ff7478f9cfe425591f57716
Maythiwat Chomchuen <maythiwat.chom@gmail.com>
2018-08-19 13:29:54 +0700

    Update creditor.php

--- examples/creditor.php
+++ examples/creditor.php
@@ -8,7 +8,7 @@ Source: https://github.com/DemzaHQ/TMNWallet
 Website: https://www.demzahq.com / https://demza.info
 */
 
-require_once 'TMNWallet.php';
+require_once '../src/TMNWallet.php';
 date_default_timezone_set('Asia/Bangkok');
 
 $wallet = new TMNWallet('your@email.com', 'y0ur_p@$$w0rd');
